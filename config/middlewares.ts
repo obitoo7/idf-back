121f885028e5d9c0012e383c4373b399a13603b8
shivam <shivampal7207@gmail.com>
2023-02-05 13:17:31 +0530

    --

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -8,6 +8,7 @@ export default [
         directives: {
           "script-src": ["'self'", "*.tinymce.com", "*.tiny.cloud", "https:"],
           "connect-src": ["'self'", "*.tinymce.com", "*.tiny.cloud", "blob:"],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
           "img-src": [
             "'self'",
             "*.tinymce.com",
@@ -17,6 +18,7 @@ export default [
             "cdn.jsdelivr.net",
             "strapi.io",
             "s3.amazonaws.com",
+            'res.cloudinary.com'
           ],
           "style-src": [
             "'self'",
