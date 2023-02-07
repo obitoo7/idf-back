3148c01bee31e94059f1031d3413253c2a00c6fe
shivam <shivampal7207@gmail.com>
2023-02-07 23:03:37 +0530

    test

--- config/database.ts
+++ config/database.ts
@@ -2,12 +2,12 @@ export default ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('MYSQLHOST', '127.0.0.1'),
-      port: env.int('MYSQLPORT', 5432),
-      database: env('MYSQLDATABASE', 'strapi'),
-      user: env('MYSQLUSER', 'strapi'),
-      password: env('MYSQLPASSWORD', 'password'),
-      ssl: env.bool(true),
+      host: env('MYSQLHOST', 'localhost'),
+      port: env.int('MYSQLPORT',),
+      database: env('MYSQLDATABASE', 'idf_blog'),
+      user: env('MYSQLUSER', 'root'),
+      password: env('MYSQLPASSWORD', ''),
+      ssl: false,
     },
   },
 });
